05b293a5cabc05670bb39f1cb34e6f345cf03ec7
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-12 22:52:35 +0200

    Initial Commit

---
+++ Shared/VPNUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPNUIApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 12/09/21.
+//
+
+import SwiftUI
+
+@main
+struct VPNUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
